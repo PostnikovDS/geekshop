482308c96a58a2493f2188c950283e126ba3b23c
Helmar <postnikovden@mail.ru>
2022-04-18 19:02:19 +0500

    lesson_10. User's profile edit

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -22,9 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-a(0coas9ht^*)ubq=b5_zw@28vp%^lml6td03oiut=5b7g5h@y"
-)
+SECRET_KEY = "django-insecure-a(0coas9ht^*)ubq=b5_zw@28vp%^lml6td03oiut=5b7g5h@y"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -184,9 +182,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 
-with open(
-    os.path.join(BASE_DIR, "tmp", "secrets", "github.json"), "r"
-) as secrets:
+with open(os.path.join(BASE_DIR, "tmp", "secrets", "github.json"), "r") as secrets:
     github_auth = json.load(secrets)
 
 SOCIAL_AUTH_GITHUB_KEY = github_auth["client_id"]
